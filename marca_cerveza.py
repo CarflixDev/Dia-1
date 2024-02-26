880068c0ee8e79e3b74e46e020a91cc10ca0d3f3
CarReel <145766484+CarflixDev@users.noreply.github.com>
2024-02-26 21:09:27 +0100

    Add files via upload

---
+++ marca_cerveza.py
@@ -0,0 +1,11 @@
+# Programa para crear la marca de cerveza
+# Fecha: 12/02/2024
+# Creador: CarReel
+
+palabra1 = input("¿Qué ciudad te gustaría visitar? : ")
+palabra2 = input("¿Cuál es tu color favorito? :  ")
+
+# Concatena las variables con saltos de línea
+marca_cerveza = "El nombre de tu marca de cerveza es: '" + "\n" + palabra1 + " " + "\n" + palabra2 + "\n" + "Grande Felicidades'"
+
+print(marca_cerveza)
